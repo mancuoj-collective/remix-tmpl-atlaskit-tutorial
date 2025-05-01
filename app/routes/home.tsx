42ce7fc1d3d52754c29aa9f9eecb3756124ed357
mancuoj <mancuoj@gmail.com>
2025-05-01 23:21:09 +0800

    chore: check

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,9 @@
+import { cn } from '@/lib/utils'
 import type { ReactElement } from 'react'
 import type { Route } from './+types/home'
-import { cn } from '@/lib/utils'
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: 'React Router' },
-    { name: 'description', content: 'Welcome to React Router!' },
-  ]
+  return [{ title: 'React Router' }, { name: 'description', content: 'Welcome to React Router!' }]
 }
 
 type PieceProps = {
@@ -14,37 +11,37 @@ type PieceProps = {
   alt: string
 }
 
-function Piece({image, alt}: PieceProps) {
+function Piece({ image, alt }: PieceProps) {
   return (
-    <img src={image} alt={alt} className="size-10 shadow p-2 rounded bg-base-100 hover:bg-base-200" draggable={false} />
+    <img
+      src={image}
+      alt={alt}
+      className="size-10 shadow p-2 rounded bg-base-100 hover:bg-base-200"
+      draggable={false}
+    />
   )
 }
 
 function King() {
-  return (
-    <Piece image='/king.png' alt="king" />
-  )
+  return <Piece image="/king.png" alt="king" />
 }
 
 function Pawn() {
-  return (
-    <Piece image='/pawn.png' alt="pawn" />
-  )
+  return <Piece image="/pawn.png" alt="pawn" />
 }
 
 type PieceType = 'king' | 'pawn'
 type Coord = [number, number]
 
-type PieceRecord  = {
+type PieceRecord = {
   type: PieceType
   location: Coord
 }
 
-export const pieceLookup: {[Key in PieceType]: () => ReactElement} = {
-	king: () => <King />,
-	pawn: () => <Pawn />,
-};
-
+export const pieceLookup: { [Key in PieceType]: () => ReactElement } = {
+  king: () => <King />,
+  pawn: () => <Pawn />,
+}
 
 function isEqualCoord(c1: Coord, c2: Coord) {
   return c1[0] === c2[0] && c1[1] === c2[1]
@@ -55,16 +52,19 @@ function renderSquares(pieces: PieceRecord[]) {
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
       const squaredCoord: Coord = [i, j]
-      const piece = pieces.find(p => isEqualCoord(p.location, squaredCoord))
-      const isDark = (i+j) % 2 === 1
+      const piece = pieces.find((p) => isEqualCoord(p.location, squaredCoord))
+      const isDark = (i + j) % 2 === 1
 
       squares.push(
-        <div key={`${i}-${j}`} className={cn(
-          'size-full bg-base-100 flex items-center justify-center',
-          isDark && 'bg-base-300'
-        )}>
+        <div
+          key={`${i}-${j}`}
+          className={cn(
+            'size-full bg-base-100 flex items-center justify-center',
+            isDark && 'bg-base-300',
+          )}
+        >
           {piece && pieceLookup[piece.type]()}
-        </div>
+        </div>,
       )
     }
   }
@@ -73,9 +73,9 @@ function renderSquares(pieces: PieceRecord[]) {
 
 function ChessBoard() {
   const pieces: PieceRecord[] = [
-		{ type: 'king', location: [3, 2] },
-		{ type: 'pawn', location: [1, 6] },
-	];
+    { type: 'king', location: [3, 2] },
+    { type: 'pawn', location: [1, 6] },
+  ]
 
   return (
     <div className="grid grid-cols-8 grid-rows-8 size-[500px] border border-base-300">
