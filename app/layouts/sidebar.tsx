a696421fc7a2e04e11e25735318a8f420a518d42
mancuoj <mancuoj@gmail.com>
2025-05-02 22:57:56 +0800

    nav link

--- app/layouts/sidebar.tsx
+++ app/layouts/sidebar.tsx
@@ -1,29 +1,28 @@
 import { cn } from '@/lib/utils'
-import { AppleIcon, CherryIcon } from 'lucide-react'
+import { AppleIcon, CherryIcon, CitrusIcon } from 'lucide-react'
 import { NavLink, Outlet } from 'react-router'
 
 const links = [
   { to: '/', icon: <AppleIcon className="size-4.5" />, label: 'Chess' },
+  { to: '/list', icon: <CitrusIcon className="size-4.5" />, label: 'List' },
   { to: '/board', icon: <CherryIcon className="size-4.5" />, label: 'Board' },
 ]
 
 export default function SidebarLayout() {
   return (
-    <div className="flex flex-col gap-6 items-center p-20 min-h-svh">
+    <div className="flex flex-col gap-7 items-center p-20 min-h-svh">
       <ul className="menu menu-horizontal bg-base-300 rounded-box gap-1.5">
         {links.map(({ to, icon, label }) => (
           <li key={label}>
             <NavLink
               to={to}
-              className={({ isActive, isPending }) =>
-                cn(
-                  'tooltip tooltip-right',
-                  isActive ? 'bg-base-100' : isPending ? 'opacity-60 bg-base-100' : '',
-                )
+              className={({ isActive }) =>
+                cn(isActive ? 'bg-base-100' : '')
               }
-              data-tip={label}
+              viewTransition
             >
               {icon}
+              {label}
             </NavLink>
           </li>
         ))}
--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,9 @@
 import { type RouteConfig, index, layout, route } from '@react-router/dev/routes'
 
 export default [
-  layout('layouts/sidebar.tsx', [index('routes/home.tsx'), route('board', 'routes/board.tsx')]),
+  layout('layouts/sidebar.tsx', [
+    index('routes/home.tsx'),
+    route('list', 'routes/list.tsx'),
+    route('board', 'routes/board.tsx'),
+  ]),
 ] satisfies RouteConfig
